33bd1ddf342df7cc1e9eb31afcd41bae6f8a6f64
Adly2020 <72651318+Adly2020@users.noreply.github.com>
2024-11-17 19:13:10 +0100

    Update script.sh

--- script.sh
+++ script.sh
@@ -11,52 +11,70 @@ vnet3="VNet3"
 subnet1="Subnet1"
 subnet2="Subnet2"
 subnet3="Subnet3"
-vm_size="Standard_B1s"
+vm_size="Standard_DS2_v2"
+image_linux="Ubuntu2204"
+image_winserver="Win2022Datacenter"
 admin_username="azureuser"
-admin_password="P@ssw0rd1234!"
+admin_password="Administrator/"
 
 # Créer le groupe de ressources
 az group create --name $resource_group --location $location
 
 # Créer la première VNet et deux VM Ubuntu
-az network vnet create --resource-group $resource_group --name $vnet1 --address-prefix 10.0.0.0/16 --subnet-name $subnet1 --subnet-prefix 10.0.1.0/24
+az network vnet create --resource-group $resource_group --name $vnet1 --address-prefix 10.1.0.0/16 --subnet-name $subnet1 --subnet-prefix 10.1.0.0/24
 
 for i in 1 2; do
     az vm create \
         --resource-group $resource_group \
         --name UbuntuVM${i} \
-        --image UbuntuLTS \
+        --image $image_linux \
         --size $vm_size \
         --vnet-name $vnet1 \
         --subnet $subnet1 \
         --admin-username $admin_username \
-        --admin-password $admin_password
+        --admin-password $admin_password \
+        --generate-ssh-keys 
 done
+# Done
+echo '--------------------------------------------------------'
+echo '             Vnet 1 et VM Setup Completed'
+echo '--------------------------------------------------------'
 
 # Créer la deuxième VNet et une VM Ubuntu
-az network vnet create --resource-group $resource_group --name $vnet2 --address-prefix 10.1.0.0/16 --subnet-name $subnet2 --subnet-prefix 10.1.1.0/24
+az network vnet create --resource-group $resource_group --name $vnet2 --address-prefix 10.2.0.0/16 --subnet-name $subnet2 --subnet-prefix 10.2.0.0/24
 
 az vm create \
     --resource-group $resource_group \
     --name UbuntuVM3 \
-    --image UbuntuLTS \
+    --image $image_linux \
     --size $vm_size \
     --vnet-name $vnet2 \
     --subnet $subnet2 \
     --admin-username $admin_username \
-    --admin-password $admin_password
+    --admin-password $admin_password \
+    --generate-ssh-keys 
+
+# Done
+echo '--------------------------------------------------------'
+echo '             Vnet 2 et VM Setup Completed'
+echo '--------------------------------------------------------'
 
 # Créer la troisième VNet et une VM Windows Server 2022 Datacenter
-az network vnet create --resource-group $resource_group --name $vnet3 --address-prefix 10.2.0.0/16 --subnet-name $subnet3 --subnet-prefix 10.2.1.0/24
+az network vnet create --resource-group $resource_group --name $vnet3 --address-prefix 10.3.0.0/16 --subnet-name $subnet3 --subnet-prefix 10.3.0.0/24
 
 az vm create \
     --resource-group $resource_group \
     --name WindowsVM \
-    --image Win2022Datacenter \
+    --image $image_winserver \
     --size $vm_size \
     --vnet-name $vnet3 \
     --subnet $subnet3 \
     --admin-username $admin_username \
     --admin-password $admin_password
+
+# Done
+echo '--------------------------------------------------------'
+echo '             Vnet 3 et VM Setup Completed'
+echo '--------------------------------------------------------'
 
 echo "Toutes les machines virtuelles ont été créées avec succès."
